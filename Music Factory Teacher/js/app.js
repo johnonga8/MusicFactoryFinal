c76adbd5cd8dfdc47e07a657c3919e276876a1fa
John Ong <john.ong@a8.sg>
2015-06-22 06:53:58 +0000

    Changes made by John Ong (john.ong@a8.sg).
    Added ajax call

--- Music Factory Teacher/js/app.js
+++ Music Factory Teacher/js/app.js
@@ -1,9 +1,7 @@
 (function () {
-    var asidemain = $(window).height();
 
-    var app = new kendo.mobile.Application(document.body,
-    {
-        transition:'slide'
-    });
+    var app = new kendo.mobile.Application(document.body);
+   
 
+  
 }());
