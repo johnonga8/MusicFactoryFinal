53cefb0efe86942bb3a2b21afefc492d3f0a3d27
John Ong <john.ong@a8.sg>
2015-06-24 11:05:08 +0800

    Pre-Alpha v1.2
    minimal transitions

--- Music Factory Teacher/js/bindings.js
+++ Music Factory Teacher/js/bindings.js
@@ -1,6 +1,6 @@
 (function () {
-    //var domain = "http://musicfactory.a8hosting.com/";
-    var domain = "http://192.168.1.39:2580/";
+    var domain = "http://musicfactory.a8hosting.com/";
+    //var domain = "http://192.168.1.39:2580/";
     var GetAllBooks = (function(){
         $.ajax({
             type: "POST",
