28087ffbeab739170779bf015ff63c8e23c6e61a
John Ong <john.ong@a8.sg>
2015-07-03 10:49:29 +0800

    bookcover error

--- Music Factory Teacher/js/bookcover.js
+++ Music Factory Teacher/js/bookcover.js
@@ -8,7 +8,3 @@ DropNav();
 //FULL HEIGHT
 var bookdetailsfullheightcover = $(window).height();
 $('.js-fullheight-cover').css('min-height', (bookdetailsfullheightcover - 140));
-(function(){
-//INITIATE APP
-  var app = new kendo.mobile.Application(document.body);
-}());
