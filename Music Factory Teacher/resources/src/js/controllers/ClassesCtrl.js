54c587697d0178937d756dce44d97e566fdc7dd6
Justin de Leon <jusmdeleon@gmail.com>
2015-09-11 19:09:33 +0800

    Add base controller for app

--- Music Factory Teacher/resources/src/js/controllers/ClassesCtrl.js
+++ Music Factory Teacher/resources/src/js/controllers/ClassesCtrl.js
@@ -1,13 +1,2 @@
-app.controller('ClassesCtrl', ['apiClass', function (apiClass) {
-	var vm = this;
-	vm.teacher = {};
-
-	activate();
-
-	////////////////////
-	function activate () {
-		apiClass.getDetails().then(function(response) {
-			vm.teacher = response.data.d;
-		});
-	}
-}]);
+app.controller('ClassesCtrl', function () {
+});
