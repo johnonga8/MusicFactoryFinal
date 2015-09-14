7900d37c2deb326118678330a3225bee20dd80eb
Justin de Leon <jusmdeleon@gmail.com>
2015-09-14 16:27:03 +0800

    Bind actual data on 'my classes ' page

--- Music Factory Teacher/www/js/musicfactory.js
+++ Music Factory Teacher/www/js/musicfactory.js
@@ -48,9 +48,11 @@ app.controller('ClassCtrl', ['apiClass', '$stateParams', function (apiClass, $st
 
 	////////////////////
 	function activate () {
-		var id = $stateParams.classId;
+		// var id = $stateParams.classId;
+		var id = "6c227db3-986e-67b9-8ff0-ff0000d15b77"; // Mocked
 		apiClass.getDetails(id).then(function(response) {
 			vm.class = response.data.d;
+			console.log(vm.class);
 		});
 	}
 }]);
